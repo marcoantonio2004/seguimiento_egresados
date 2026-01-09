43729323b6654442ca8433f14bb3e71cde1ac257
marcoantonio <marooagc04@gmail.com>
2026-01-09 13:34:44 -0600

    update

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, redirect, url_for, session
 from auth import auth_bp
 from egresados import egresados_bp
+from db import get_connection
+import psycopg2.extras
 
 app = Flask(__name__)
 app.secret_key = "clave_secreta_egresados"
@@ -12,11 +14,43 @@ app.register_blueprint(egresados_bp)
 def index():
     return render_template("index.html")
 
+# =========================
+# DASHBOARD
+# =========================
 @app.route("/dashboard")
 def dashboard():
     if "usuario" not in session:
         return redirect(url_for("auth.login"))
-    return render_template("dashboard.html")
+
+    conn = get_connection()
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+    # Alumnos totales
+    cur.execute("SELECT COUNT(*) FROM egresados")
+    alumnos_totales = cur.fetchone()[0]
+
+    # Egresados
+    cur.execute("SELECT COUNT(*) FROM egresados WHERE estatus = 'Egresado'")
+    total_egresados = cur.fetchone()[0]
+
+    # Titulados
+    cur.execute("SELECT COUNT(*) FROM egresados WHERE estatus = 'Titulado'")
+    total_titulados = cur.fetchone()[0]
+
+    # En seguimiento
+    cur.execute("SELECT COUNT(*) FROM egresados WHERE estatus = 'En seguimiento'")
+    total_seguimiento = cur.fetchone()[0]
+
+    cur.close()
+    conn.close()
+
+    return render_template(
+        "dashboard.html",
+        alumnos_totales=alumnos_totales,
+        total_egresados=total_egresados,
+        total_titulados=total_titulados,
+        total_seguimiento=total_seguimiento
+    )
 
 if __name__ == "__main__":
     app.run(debug=True)
