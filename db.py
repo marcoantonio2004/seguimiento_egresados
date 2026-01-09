ee8d831ff383a12443f727f883739f3991aefbbc
marcoantonio <marooagc04@gmail.com>
2026-01-08 22:00:04 -0600

    Update db

--- db.py
+++ db.py
@@ -2,7 +2,7 @@ import psycopg2
 import psycopg2.extras
 import os
 
-DATABASE_URL = os.environ.get("postgresql://postgres:igtByEgYBKnvijJVXtZjhXQIeixcKmlU@tramway.proxy.rlwy.net:21961/railway")
+DATABASE_URL = os.environ.get("DATABASE_PUBLIC_URL")
 
 def get_connection():
     conn = psycopg2.connect(DATABASE_URL)
